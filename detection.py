b754cfe2acea7a2982e7a28da31d4562df26be1b
brostudio000 <hashfifahl@gmail.com>
2022-06-19 14:59:41 +0700

    Update code

--- detection.py
+++ detection.py
@@ -1,43 +1,45 @@
-import cv2 
+import cv2 as cv
 import torch
-import numpy as np
+import numpy as np 
 
-# Model
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
-model.img =360
-cap = cv2.VideoCapture(1)
+# Model yang digunakan untuk deteksi
+path = "best_result14.pt"
+model = torch.hub.load('', 'custom', path=path, source='local')
+model.img = 480
+# Source yang digunakan untuk deteksi
+cap = cv.VideoCapture(1)
 if (cap.isOpened()== False): 
   print("Error opening video stream or file")
 
-# Baca sampai selesai
+# Baca source sampai selesai
 while(cap.isOpened()):
   # Capture frame-by-frame
   ret, frame = cap.read()
   if ret == True:
-    results = model(frame)
-    DetectedBoxes =results.pandas().xyxy[0].values.tolist()
-    #---Start  for Box...
-    for Box in DetectedBoxes:
+    result = model(frame) # Inference
+
+    DetectionBoxes = result.pandas().xyxy[0].values.tolist()
+    for Box in DetectionBoxes:
+      # Bounding Box
       xmin,ymin,xmax,ymax,conf,cl,name = Box
       pmin =(int(xmin),int(ymin))
       pmax =(int(xmax),int(ymax))
-      frame=cv2.rectangle(frame ,pmin,pmax,(0,255,255),2)
+      frame = cv.rectangle(frame, pmin, pmax,(255,255,0),3)
       pPos = (int(xmin),int(ymin))
-      # fontScale
-      fontScale = 0.8
+      # Font
+      fontScale = 2
       color = (255, 255, 255)
       thickness = 2
-      font = cv2.FONT_HERSHEY_SIMPLEX
-      frame = cv2.putText(frame,name, pPos, font,fontScale, color,thickness, cv2.LINE_AA)
-    #---end for Box...
-
-    cv2.imshow('Frame',frame)
-    # Tekan Tombol Q Untuk Keluar
-    if cv2.waitKey(25) & 0xFF == ord('q'):
+      font = cv.FONT_HERSHEY_DUPLEX
+      frame = cv.putText(frame,name, pPos, font,fontScale, color,thickness, cv.LINE_AA)
+    # Menampilkan window capture
+    cv.imshow('Object Detection',frame)
+    # Tombol Q untuk keluar dari window capture
+    if cv.waitKey(25) & 0xFF == ord('q'):
       break
   # Keluar dari loop
   else: 
     break
 cap.release()
 # Hapus Semua frame
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
